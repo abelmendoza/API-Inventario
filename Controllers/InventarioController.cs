3c768cd903f68f7ccaa4a4a60ecb44efa25fc639
ABEL MENDOZA <mendozaabel66@gmail.com>
2021-06-11 21:01:19 -0400

    the controller was added through the context variable

---
+++ Controllers/InventarioController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiInventario.Context;
+
+
+namespace ApiInventario.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventarioController : ControllerBase
+    {
+        private readonly AppDbContext context;
+        public InventarioController(AppDbContext _context)
+        {
+            this.context = _context;
+        }
+        [HttpGet]
+        public ActionResult GetAll()
+
+        {
+            // Error handling Try-catch
+            return Ok(context.inventario.ToList());
+        }
+    }
+}
