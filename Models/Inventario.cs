e71f3255584ef3c9e1eec904b31b2bb38735a47d
ABEL MENDOZA <mendozaabel66@gmail.com>
2021-06-11 20:25:09 -0400

    add propertys

--- Models/Inventario.cs
+++ Models/Inventario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,14 @@ namespace ApiInventario.Models
 {
     public class Inventario
     {
+        [Key]
+        public int id { get; set; }
+        public string dimensionAncho { get; set; }
+        public string dimensionLargo { get; set; }
+        public string tipoTela { get; set; }
+        public string color { get; set; }
+        public string procedencia { get; set; }
+        public string marca { get; set; }
+        public bool estado { get; set; }
     }
 }
